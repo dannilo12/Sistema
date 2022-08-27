81f90264b603521c3fd7d6e70d3d181186097e53
dannilo12 <danteking1@gmail.com>
2022-08-27 15:25:24 -0300

    criando class da pessoa jurídica sa4

---
+++ PessoaJuridica.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema{
+    public class pessoaJuridica : pessoa{
+        public string? CNPJ { get; set; }
+        public string? RazaoSocial { get; set; }
+        
+        
+    }
+}
